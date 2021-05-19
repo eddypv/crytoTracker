d7f96c3d40b9083808fa1743ce950083907efad7
Eddy <eddypv0@gmail.com>
2021-05-18 22:19:45 -0400

    Create Screen Coin Detail

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,13 +1,40 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-class CoinsScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Coins Screen</Text>
-      </View>
-    );
-  }
-}
+import React from 'react';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+
+const CoinsScreen = props => {
+  const handlerPress = () => {
+    console.log('On press', props);
+    props.navigation.navigate('CoinDetail');
+  };
+  return (
+    <View style={style.container}>
+      <Text style={style.title}>Coins Screen</Text>
+      <Pressable style={style.btn} onPress={handlerPress}>
+        <Text style={style.btnText}>Ir a detail</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  btn: {
+    padding: 8,
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    margin: 16,
+  },
+  btnText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
 
 export default CoinsScreen;
